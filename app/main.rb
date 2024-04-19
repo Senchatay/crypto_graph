c362534e8bbf8a4e4eb5a4667174380468e52120
Arseniy <95499171+Senchatay@users.noreply.github.com>
2024-04-19 21:03:07 +0300

    feature-2: Calculate cost with blockchain-system comission. (#2)

--- app/main.rb
+++ app/main.rb
@@ -2,8 +2,11 @@
 
 require 'require_all'
 require 'byebug'
-require_all('app/services/**/*.rb')
+require_all('app/services/finder/*.rb')
+require_all('app/services/presenter/*.rb')
 require_all('app/models/graph/**/*.rb')
 require_all('app/models/exchange/**/*.rb')
+require_all('app/services/loader/*.rb')
+require_all('app/services/*.rb')
 
 Presenter::TxtResult.call(RateToGraph.call)
